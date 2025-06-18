2132101a2df57acec32354d46cfd6bfb301ff2b7
Miloš MLAĐEN <milosmladjen85+1@gmail.com>
2025-06-18 06:06:59 +0200

    commit change

--- Databricks-case/3. ingestion/2. ingest customer_to_csv file.py
+++ Databricks-case/3. ingestion/2. ingest customer_to_csv file.py
@@ -1,51 +1,29 @@
 # Databricks notebook source
-# MAGIC %md
-# MAGIC Step 1 : Ingest customer_to_csv file from Azure Data Lake 
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC Read the CSV file using DataFrame Reader API
-
-# COMMAND ----------
+#  Step 1 : Ingest customer_to_csv file from Azure Data Lake 
+# Read the CSV file using DataFrame Reader API
 
 display(dbutils.fs.mounts())
 
-# COMMAND ----------
+# %fs
+# ls /mnt/getcase1dl/raw
 
-# MAGIC %fs
-# MAGIC ls /mnt/getcase1dl/raw
-
-# COMMAND ----------
 
 customer_df = spark.read.csv("/mnt/getcase1dl/raw/customer_to_csv.csv")
 customer_df.show()
-
-# COMMAND ----------
-
 display(customer_df)
-
-# COMMAND ----------
 
 customer_df = spark.read.option("header",True).csv("/mnt/getcase1dl/raw/customer_to_csv.csv")
 display(customer_df)
-
-# COMMAND ----------
-
 customer_df.printSchema()
 
-# COMMAND ----------
 
 customer_df = spark.read.option("header",True)\
     .option("inferSchema", True)\
     .csv("/mnt/getcase1dl/raw/customer_to_csv.csv")
 customer_df.printSchema()
 
-# COMMAND ----------
 
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, StringType, DoubleType
-
-# COMMAND ----------
 
 customer_schema = StructType(fields =[StructField("CustomerID", IntegerType(), False),
                                     StructField("CompanyName", StringType(), True),
@@ -60,56 +38,32 @@ customer_schema = StructType(fields =[StructField("CustomerID", IntegerType(), F
                                     StructField("StateProvince", StringType(), True),
                                     ])
 
-# COMMAND ----------
 
 customer_df = spark.read.option("header", True)\
     .schema(customer_schema)\
     .csv("/mnt/getcase1dl/raw/customer_to_csv.csv")
 
-# COMMAND ----------
-
 customer_df.printSchema()
-
-# COMMAND ----------
-
 display(customer_df)
 
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC Step 2 : Select only the required Columns
-
-# COMMAND ----------
+#  Step 2 : Select only the required Columns
 
 customer_selected_df = customer_df.select("CustomerID", "CompanyName", "ContactName", "City", "Country", "DivisionID")
 display(customer_selected_df)
 
-# COMMAND ----------
-
 #another way of selecting the required columns 
 customer_selected_df = customer_df.select(customer_df.CustomerID, customer_df.CompanyName, customer_df.ContactName, customer_df.City, customer_df.Country, customer_df.DivisionID)
-
-# COMMAND ----------
 
 #third way of selecting the required columns 
 customer_selected_df = customer_df.select(customer_df["CustomerID"], customer_df["CompanyName"], customer_df["ContactName"], customer_df["City"], customer_df["Country"], customer_df["DivisionID"])
 
-# COMMAND ----------
 
 from pyspark.sql.functions import col
-
-
-# COMMAND ----------
-
 customer_selected_df = customer_df.select(col("CustomerID"), col("CompanyName"), col("ContactName"), col("City"), col("Country"), col("DivisionID"))
 display(customer_selected_df)
 
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC Step 3: Preimenovanje kolona radi kasnije analize podataka u SQL-u i Python-u (nazivi varijabli u Python standardu, i da bi se izbeglo prilikom pisanja sql query-a da dodje do zabune ukoliko neko bude pisao imena sve lower-case)
-
-# COMMAND ----------
+#  Step 3: Column renaming for further data analysis in SQL and Python (variable names kept in Python standard, 
+#  in order to avoid potential misunderstandings and errors when creating the scripts in SQL with lower-case naming standard)
 
 customer_renamed_df = customer_selected_df.withColumnRenamed("CustomerID","customer_id")\
     .withColumnRenamed("CompanyName","company_name")\
@@ -117,73 +71,29 @@ customer_renamed_df = customer_selected_df.withColumnRenamed("CustomerID","custo
             .withColumnRenamed("City","city")\
                 .withColumnRenamed("Country","country")\
                     .withColumnRenamed("DivisionID","division_id")
-
-# COMMAND ----------
-
 display(customer_renamed_df)
-
-# COMMAND ----------
 
 from pyspark.sql.functions import current_timestamp , lit
 
-# COMMAND ----------
-
-# MAGIC %md 
-# MAGIC 4. Adding new Column to DataFrame with "withColumn()" function 
-
-# COMMAND ----------
-
+#  Adding new Column to DataFrame with "withColumn()" function 
 customer_final_df = customer_renamed_df.withColumn("ingestion_date", current_timestamp())
-
-# COMMAND ----------
-
 display(customer_final_df)
 
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC Step 4 : option with addign literal value into a column inside the DataFrame 
-
-# COMMAND ----------
-
+#  Step 4 : option with addign literal value into a column inside the DataFrame 
 customer_final_df = customer_renamed_df.withColumn("env", lit("Production"))
 display(customer_final_df)
 
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC Step 5: Write data to Datalake as parquet
-
-# COMMAND ----------
-
+#  Step 5: Write data to Datalake as parquet
 display(customer_final_df)
-
-# COMMAND ----------
-
 customer_final_df.write.mode("overwrite").parquet("/mnt/getcase1dl/processed/customer")
-
-# COMMAND ----------
-
 customer_final_df.write.mode("overwrite").format("parquet").saveAsTable("acme_processed.customer")
 
-# COMMAND ----------
-
-# MAGIC %fs
-# MAGIC ls /mnt/getcase1dl/processed/customer
-
-# COMMAND ----------
+#  %fs
+#  ls /mnt/getcase1dl/processed/customer
 
 df = spark.read.parquet("/mnt/getcase1dl/processed/customer")
-
-# COMMAND ----------
-
 display(df)
-
-# COMMAND ----------
-
 display(spark.read.parquet("/mnt/getcase1dl/processed/customer"))
 
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC SELECT * FROM acme_processed.customer
+#  %sql
+#  SELECT * FROM acme_processed.customer
